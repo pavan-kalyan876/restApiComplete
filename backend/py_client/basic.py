ee8044a43a6e5c327a6ff4d767868482b317ff06
pavansta <pavan.learn69@gmail>
2024-10-02 19:01:18 +0530

    checking how it will be working while using httpResponce

--- backend/py_client/basic.py
+++ backend/py_client/basic.py
@@ -1,9 +1,10 @@
 import requests
 
-#endpoint = "https://httpbin.org/status/200"
-#endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api"
-get_response = requests.get(endpoint,params={"abc":123}, json={"data": "hello world"})
-#print(get_response.text)
-print(get_response.json())
+get_response = requests.get(endpoint, params={"abc": 123}, json={"data": "hello world"})
+print(get_response.text)
+print(get_response.headers)
+# print(get_response.json())
 print(get_response.status_code)
