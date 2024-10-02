f6d9570420bed1bab935139a208529d6338ea72d
pavansta <pavan.learn69@gmail>
2024-10-02 09:16:31 +0530

    implemented sample to display status of the code or url

--- backend/py_client/basic.py
+++ backend/py_client/basic.py
@@ -1,7 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
+#endpoint = "https://httpbin.org/status/200"
 endpoint = "https://httpbin.org/anything"
 get_response = requests.get(endpoint, json={"data": "hello world"})
 print(get_response.text)
 print(get_response.json())
+print(get_response.status_code)
