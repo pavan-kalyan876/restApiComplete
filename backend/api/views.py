f2f56d2d033f289260896192ea88f78bcbfdd5e9
pavansta <pavan.learn69@gmail>
2024-10-02 09:41:02 +0530

    succesfully run api vies in broswer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def api_home(request, *args, **kwargs):
+    return JsonResponse({"message": "hi there this your django api response"})
