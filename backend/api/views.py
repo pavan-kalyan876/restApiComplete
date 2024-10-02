93e43c260eca4504d0591937acac144e3832142e
pavansta <pavan.learn69@gmail>
2024-10-02 10:05:03 +0530

    getting data from client to server displayed succesfully in terminal

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hi there this your django api response"})
+    body = request.body  # byte json data
+    print(request.GET)
+    print(request.POST)
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data.keys())
+    
+    return JsonResponse(data)
--- backend/py_client/basic.py
+++ backend/py_client/basic.py
@@ -2,8 +2,8 @@ import requests
 
 #endpoint = "https://httpbin.org/status/200"
 #endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000"
-get_response = requests.get(endpoint, json={"data": "hello world"})
-print(get_response.text)
-#print(get_response.json())
+endpoint = "http://127.0.0.1:8000/api"
+get_response = requests.get(endpoint,params={"abc":123}, json={"data": "hello world"})
+#print(get_response.text)
+print(get_response.json())
 print(get_response.status_code)
