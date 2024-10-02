888a4815067be18c922bd438f0975b49633074ec
pavansta <pavan.learn69@gmail>
2024-10-02 18:55:59 +0530

    updated feilds, whatever is there in feild it will that data only in output other field will be hided

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 import json
 from products.models import Product
 
@@ -10,9 +11,6 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data["id"] = model_data.id
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
+        data = model_to_dict(model_data, fields=["id", "title",])
 
     return JsonResponse(data)
