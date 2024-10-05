7dba926e92a7b53662cd8a7eec1c5557e1441bc3
pavansta <pavan.learn69@gmail>
2024-10-05 10:09:55 +0530

    created create.py in pycliend added some data displaing same file working properly and added data using create view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+# api/products
 urlpatterns = [
+    path("", views.product_create_view),
     path("<int:pk>/", views.ProductDetailAPIView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -6,9 +6,15 @@ from .serializers import ProductSerializer
 # Create your views here.
 
 
+class ProductCreateAPI(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# as_view it converts class based view into a function based
+product_create_view = ProductCreateAPI.as_view()
+
+
 # it retrieves only single object
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-    
