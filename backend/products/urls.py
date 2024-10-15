35ef6c63937af902b97079189028b4b35e194425
pavansta <pavan.learn69@gmail>
2024-10-15 09:54:09 +0530

    changes done in urls according to generics class based view,leaving function based things

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 # api/products
 urlpatterns = [
-    path("", views.product_alt_view),
-    path("<int:pk>/", views.product_alt_view),
+    path("", views.product_list_create_view),
+    path("<int:pk>/", views.product_detail_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -35,12 +35,13 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # lookup field
 
 
+product_detail_view = ProductDetailAPIView.as_view()
 
 
 # args allows a function to accept any number of positional arguments. These arguments are passed as a tuple.
 # kwargs allows a function to accept any number of keyword arguments (arguments passed as key-value pairs)
 # These arguments are passed as a dictionary
-
+# function based views
 @api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method
@@ -48,7 +49,7 @@ def product_alt_view(request, pk=None, *args, **kwargs):
     if method == "GET":
         if pk is not None:
             # Detail view
-            obj = get_object_or_404(Product, pk=pk)# If object doesn't exist raise 404
+            obj = get_object_or_404(Product, pk=pk)  # If object doesn't exist raise 404
             data = ProductSerializer(obj).data
             return Response(data)
         else:
@@ -62,7 +63,9 @@ def product_alt_view(request, pk=None, *args, **kwargs):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             title = serializer.validated_data.get("title")
-            content = serializer.validated_data.get("content", title) # Default content to title if not provided
+            content = serializer.validated_data.get(
+                "content", title
+            )  # Default content to title if not provided
             serializer.save(content=content)
-            return Response(serializer.data)  
+            return Response(serializer.data)
         return Response({"detail": "Invalid data"})
