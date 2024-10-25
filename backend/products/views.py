d37814a9c0d21092cfb4fefa1cfb0a7e0095e369
pavansta <pavan.learn69@gmail>
2024-10-25 08:14:53 +0530

    done with createMixins

--- backend/products/views.py
+++ backend/products/views.py
@@ -72,18 +72,24 @@ product_destroy_view = ProductDeleteAPIView.as_view()
 # list ListModelMixin used to retrieve all data, like get method
 # retrieveModelMixin is used to retrieve a single data or specific data
 class ProductMixins(
-    mixins.ListModelMixin, mixins.RetrieveModelMixin, generics.GenericAPIView
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView,
 ):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "pk"
 
     def get(self, request, *args, **kwargs):
-        print(args, kwargs) # it print the pk the object
+        print(args, kwargs)  # it print the pk the object
         pk = kwargs.get("pk")
         if pk is not None:
             return self.retrieve(request, *args, **kwargs)
         return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 product_mixin_view = ProductMixins.as_view()
